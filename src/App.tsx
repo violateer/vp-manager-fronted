88d6be1be2498e2ebcc0c956e463a533cb9f3b2c
violateer <1828257089@qq.com>
2024-05-15 22:26:06 +0800

    调整主题变量

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import { Suspense, defineComponent } from 'vue'
+import { Suspense, computed, defineComponent } from 'vue'
 import { RouterView } from 'vue-router'
 import { zhCN, dateZhCN } from 'naive-ui'
+import { lighten } from './utils'
 
 export default defineComponent({
   setup() {
+    /**
+     * @type import('naive-ui').GlobalThemeOverrides
+     */
+    const getThemeOverrides = computed(() => {
+      const appTheme = '#2d8cf0'
+      const lightenStr = lighten('#2d8cf0', 6)
+      return {
+        common: {
+          primaryColor: appTheme,
+          primaryColorHover: lightenStr,
+          primaryColorPressed: lightenStr,
+          primaryColorSuppl: appTheme
+        },
+        LoadingBar: {
+          colorLoading: appTheme
+        }
+      }
+    })
+
     return () => (
-      <n-config-provider locale={zhCN} date-locale={dateZhCN}>
+      <n-config-provider
+        locale={zhCN}
+        date-locale={dateZhCN}
+        theme-overrides={getThemeOverrides.value}
+      >
         <n-message-provider>
           <n-loading-bar-provider>
             <Suspense>
--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -10,9 +10,47 @@ export default defineComponent({
   setup(props, context) {
     return () => (
       <div class={s.wrapper}>
-        <n-layout embedded content-style="padding: 24px;">
-          <n-card>只要是 看到天边云一朵 逐天拢有好心情</n-card>
-        </n-layout>
+        <n-grid class={s.grid} x-gap="12" cols="3">
+          <n-gi span="1">
+            <n-card
+              size="small"
+              bordered={false}
+              segmented={{
+                content: true,
+                footer: 'soft'
+              }}
+            >
+              {{
+                header: () => (
+                  <n-space>
+                    <n-button type="info" ghost icon-placement="right">
+                      添加顶级菜单
+                    </n-button>
+                    <n-button type="info" ghost icon-placement="right">
+                      添加子菜单
+                    </n-button>
+                  </n-space>
+                ),
+                default: () => <div>卡片内容</div>
+              }}
+            </n-card>
+          </n-gi>
+          <n-gi span="2">
+            <n-card
+              size="small"
+              bordered={false}
+              segmented={{
+                content: true,
+                footer: 'soft'
+              }}
+            >
+              {{
+                header: () => <span>编辑菜单</span>,
+                default: () => <div>卡片内容</div>
+              }}
+            </n-card>
+          </n-gi>
+        </n-grid>
       </div>
     )
   }
